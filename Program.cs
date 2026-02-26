c538768486041f5684fa7c5da3e45025b6d79d46
Danilomartinsdeoliveira <danilooliveira.161103@gmail.com>
2026-02-26 14:34:03 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        using var client = new HttpClient();
+
+        var response = await client.GetAsync("https://api.adviceslip.com/advice");
+        response.EnsureSuccessStatusCode();
+
+        var json = await response.Content.ReadAsStringAsync();
+
+        using JsonDocument doc = JsonDocument.Parse(json);
+
+        string conselho = doc.RootElement
+        .GetProperty("slip")
+        .GetProperty("advice")
+        .GetString();
+
+        Console.WriteLine("Conselho de Hoje:");
+        Console.WriteLine(conselho);
+        Console.WriteLine("\nLimpando memória... fim da requisição!");
+    }
+}
